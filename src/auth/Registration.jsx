46332c3c0acb85ebdc514dbdb74a8d6cc108a711
jaysharmagithub <125895781+jaysharmagithub@users.noreply.github.com>
2024-08-17 00:57:05 +0530

    Update Registration.jsx

--- src/auth/Registration.jsx
+++ src/auth/Registration.jsx
@@ -48,7 +48,7 @@ const Registration = () => {
       <h2>Register</h2>
       <form onSubmit={handleRegistration}>
         <div className="mb-3 row">
-          <label htmlFor="firstName" className="col-sm-3 col-form-label">
+          <label htmlFor="firstName" className="col-sm-2 col-form-label">
             First Name
           </label>
           <div className="col-sm-10">
